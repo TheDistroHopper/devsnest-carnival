1667734e1e52b6462b867f60d3cb2186f3e5dd75
Sarvesh Patil <sarveshspatil111@gmail.com>
2021-07-12 00:29:00 +0530

    Complete day 12 THA

--- day_12/script.js
+++ day_12/script.js
@@ -1,25 +1,69 @@
-let add = document.getElementById("addButton");
-let newInput = document.querySelector(".newInput");
-add.addEventListener("click", takeInput);
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const workUL = document.getElementById("work");
 
-let arr = Array();
-function takeInput() {
-  let newInp = document.createElement("div");
-  newInp.classList.add("list-item");
-  newInp.innerHTML = newInp.value;
+const work = JSON.parse(localStorage.getItem("work"));
+
+if (work) {
+  work.forEach((todo) => {
+    addTodo(todo);
+  });
 }
 
-function makeItem(txt, i) {
-  return (
-    "<li>\n" +
-    txt +
-    "\n" +
-    "<button id ='del" +
-    i +
-    "'>Del</button>\n" +
-    "<button id ='done" +
-    i +
-    "'>Done</button>\n" +
-    "</li>"
-  );
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  addTodo();
+});
+
+function addTodo(todo) {
+  let todoText = input.value;
+
+  if (todo) {
+    todoText = todo.text;
+  }
+
+  if (todoText) {
+    const todoEl = document.createElement("li");
+    if (todo && todo.completed) {
+      todoEl.classList.add("completed");
+    }
+
+    todoEl.innerText = todoText;
+
+    todoEl.addEventListener("click", () => {
+      todoEl.classList.toggle("completed");
+
+      updateLS();
+    });
+
+    todoEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+
+      todoEl.remove();
+
+      updateLS();
+    });
+
+    workUL.appendChild(todoEl);
+
+    input.value = "";
+
+    updateLS();
+  }
+}
+
+function updateLS() {
+  const workEl = document.querySelectorAll("li");
+
+  const work = [];
+
+  workEl.forEach((todoEl) => {
+    work.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("work", JSON.stringify(work));
 }

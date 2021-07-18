1ae68b4ce012e4377d653554d9f2e9034e78247a
Sarvesh Patil <sarveshspatil111@gmail.com>
2021-07-18 21:27:48 +0530

    Complete day 17 THA

--- day_17/src/index.js
+++ day_17/src/index.js
@@ -1,11 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+
+
+function Item(props) {
+  return (
+      <div className='component'>
+          <h1>
+              {props.title}
+          </h1>
+          <p>
+              {'Today you consumed ' + props.desc + ' calories.'}
+          </p>
+      </div>
+  )
+}
+
+let cal_list = Array(10);
+cal_list = [
+  ['Homemade Granola', 489],
+  ['Meats (Chicken Leg)', 184],
+  ['Firm Tofu', 144],
+  ['Fish (Salmon)', 206],
+  ['Avocados', 160],
+  ['Dairy Foods (Milk)', 61],
+  ['Chickpeas (Garbanzo Beans)', 164],
+  ['Sweet Potatoes', 101],
+  ['Whole Grains (Brown Rice)', 123],
+  ['Nuts (Macadamia Nuts)', 718],
+]
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className='container'>
+            {
+                cal_list.map(cal_ar => <Item title={cal_ar[0]} desc={cal_ar[1]}/>)
+            }
+        </div>
   </React.StrictMode>,
   document.getElementById("root")
 );

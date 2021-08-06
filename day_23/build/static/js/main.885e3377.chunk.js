76a47e3a832f1407c6d3441c58eb6951dd86880c
Sarvesh Patil <sarveshspatil111@gmail.com>
2021-08-06 23:58:27 +0530

    Started day 23 THA

---
+++ day_23/build/static/js/main.885e3377.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpday_23=this.webpackJsonpday_23||[]).push([[0],{11:function(n,e,t){"use strict";t.r(e);var c=t(1),i=t.n(c),o=t(3),r=t.n(o),s=(t(8),t(9),t(0));var a=function(){return Object(s.jsx)("div",{className:"App",children:Object(s.jsx)("h1",{children:"Day 23 Commig Soon"})})};r.a.render(Object(s.jsx)(i.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},8:function(n,e,t){},9:function(n,e,t){}},[[11,1,2]]]);
+//# sourceMappingURL=main.885e3377.chunk.js.map
---
+++ day_23/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Day 23 Commig Soon</h1>
+    </div>
+  );
+}
+
+export default App;

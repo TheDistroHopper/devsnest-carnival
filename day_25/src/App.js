014c4ceb736b067a36f357160af7191c3a01f069
Sarvesh Patil <sarveshspatil111@gmail.com>
2021-08-11 21:56:51 +0530

    No THA on day 25

---
+++ day_25/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>No THA on day 25</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
